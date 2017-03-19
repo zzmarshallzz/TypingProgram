ec8bda0021f1eaddf88209e8f80c4056c57fad98
Marshall Luu <zzmarshallzz14@hotmail.com>
2017-03-19 17:38:21 -0500

    Add files via upload

---
+++ AppStart.java
@@ -0,0 +1,17 @@
+
+public class AppStart extends GUI {
+
+	static long start = System.currentTimeMillis();
+
+	// Main method
+	public static void main(String[] args) {
+
+		GUI go = new GUI();
+
+		go.setTitle("Typing Test");
+		go.setVisible(true);
+	};
+
+	// timer.start;
+
+}
---
+++ GUI.java
@@ -0,0 +1,154 @@
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GUI extends JFrame implements KeyListener {
+
+	// text field and label for output and input.
+	JTextField keyText = new JTextField(20);
+	JLabel keyLabel = new JLabel("Type in the correct letters");
+
+	// creating object to use method from WorldList class.
+	WordList wordListObj = new WordList();
+	String wordPulled = wordListObj.pickWord();
+
+	JLabel wordPulledLabel = new JLabel("Type in the Letters of this word: 			" + wordPulled);
+
+	// variables for the keyTyped Method within the KeyListener Interface
+	public char keyChar;
+	// Counters: i = location of which character you are on for the word at
+	// hand, ii = check against length of word, iii = iteration of
+	// checkNoNewNumbers method.
+	private int i = 0;
+	private int ii = i + 1;
+	private int iii = 0;
+
+	// Timer
+	JLabel timerLabel = new JLabel("");
+
+	// ArrayList to set Flag to not reuse same word.
+	ArrayList<String> flagNoNewNumbers = new ArrayList<String>();
+
+	// Sets up new canvas and layout
+	public GUI() {
+		keyText.addKeyListener(this);
+		setSize(400, 400);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		BorderLayout layout = new BorderLayout();
+		setLayout(layout);
+		add(keyLabel, BorderLayout.CENTER);
+		add(wordPulledLabel, BorderLayout.NORTH);
+		add(keyText, BorderLayout.SOUTH);
+		add(timerLabel, BorderLayout.EAST);
+		flagNoNewNumbers.add(wordPulled);
+	}
+
+	// Wait method
+	public static void waiting(int n) {
+		long t0, t1;
+		t0 = System.currentTimeMillis();
+		do {
+			t1 = System.currentTimeMillis();
+		} while ((t1 - t0) < (n * 1000));
+	};
+
+	// reset method
+	public void resetAll() {
+		flagNoNewNumbers.clear();
+		resetWord();
+		i=0;
+		iii=0;
+		}
+
+	// part of the interface for KeyListener, this is where the logic is
+	// executed
+	public void keyTyped(KeyEvent e) {
+		keyChar = e.getKeyChar();
+		getWord();
+	}
+
+	public void getWord() {
+
+		System.out.println(flagNoNewNumbers);
+
+		if (keyChar == wordPulled.charAt(i)) {
+
+			keyLabel.setText("You typed it correctly!");
+			System.out.println(wordPulled.length());
+			System.out.println(i);
+			System.out.println(ii);
+			System.out.println(flagNoNewNumbers);
+
+			if (ii == wordPulled.length()) {
+				resetWord();
+				keyLabel.setText("You typed it correctly!");
+				checkFinish();
+			}
+			i++;
+			ii++;
+		} else {
+			keyLabel.setText("Try another key...");
+
+		}
+		keyText.setText("");
+
+	}
+
+	// method to reset the word
+	void resetWord() {
+		i = 0;
+		ii = i + 1;
+		wordPulledLabel.setText("");
+		wordPulled = wordListObj.pickWord();
+		checkNoNewNumbers();
+		wordPulledLabel.setText("Type in the Letters of this word: 			" + wordPulled);
+		getWord();
+
+	}
+
+	// checks ArrayList to see if words were inserted.
+	void checkNoNewNumbers() {
+
+		if (flagNoNewNumbers.contains(wordPulled) && (flagNoNewNumbers.size() != wordListObj.arrLength)) {
+			resetWord();
+		} else if (flagNoNewNumbers.contains(wordPulled) && (flagNoNewNumbers.size() == wordListObj.arrLength)) {
+			wordPulledLabel.setText("You've typed all the words! Congratulations.");
+		} else {
+			flagNoNewNumbers.add(wordPulled);
+		}
+	}
+
+	void checkFinish() {
+		if (flagNoNewNumbers.contains(wordPulled) && (flagNoNewNumbers.size() == wordListObj.arrLength) && (iii == 0)) {
+			wordPulledLabel.setText("Almost there... Last word:      " + wordPulled);
+			iii++;
+		} else if (flagNoNewNumbers.contains(wordPulled) && (flagNoNewNumbers.size() == wordListObj.arrLength)
+				&& (iii == 1)) {
+			wordPulledLabel.setText("You've typed all the words! Congratulations.");
+			long end = System.currentTimeMillis();
+			long diff = (end - AppStart.start);
+			String diff2 = String.valueOf(diff);
+			timerLabel.setText("You completed in " + diff2 + " milliseconds");
+			resetAll();
+			AppStart.start = System.currentTimeMillis();
+			}
+		
+	}
+
+	// Not used
+	public void keyPressed(KeyEvent e) {
+	}
+
+	// Not used
+	public void keyReleased(KeyEvent txt) {
+	}
+}
